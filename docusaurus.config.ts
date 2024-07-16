09f77f472407c6d604342efe292e2976a502ce5f
Ulman-huy <buihuy.lh@gmail.com>
2024-07-16 10:52:19 +0700

    save

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,8 +48,6 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/Ulman-huy/docusaurus/tree/docs",
-          path: "../../",
-          include: ["**/*.md", "**/*.mdx"],
         },
         theme: {
           customCss: "./src/css/custom.css",
